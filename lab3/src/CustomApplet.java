62c22ac2f678d0b177b863cfb825d595b40d0247
NataliaZhydeikina <nata.iiinx88@gmail.com>
2022-12-04 13:45:24 +0200

    fix

--- lab3/src/CustomApplet.java
+++ lab3/src/CustomApplet.java
@@ -13,35 +13,35 @@ public class CustomApplet extends JApplet {
             private JButton reset;
 
             public CustomApplet() {
-                setBounds(300, 90, 620, 400);
+                setBounds(300, 90, 820, 400);
                 c = getContentPane();
                 c.setLayout(null);
-                title = new JLabel("Movie Magic Quiz");
+                title = new JLabel("Розрахунок фінансового показника \"Додана вартість\"");
                 title.setFont(new Font("Arial", Font.PLAIN, 30));
-                title.setSize(300, 50);
-                title.setLocation(200, 30);
+                title.setSize(800, 50);
+                title.setLocation(20, 30);
                 c.add(title);
-                name = new JLabel("Name:");
+                name = new JLabel("Вартість виробленої продукції:");
                 name.setFont(new Font("Arial", Font.PLAIN, 15));
-                name.setSize(100, 20);
-                name.setLocation(100, 100);
+                name.setSize(300, 20);
+                name.setLocation(50, 100);
                 c.add(name);
                 tname = new JTextField();
                 tname.setFont(new Font("Arial", Font.PLAIN, 15));
                 tname.setSize(250, 20);
-                tname.setLocation(250, 100);
+                tname.setLocation(350, 100);
                 c.add(tname);
-                mno = new JLabel("Favorite movie:");
+                mno = new JLabel("Вартість сировини і енергії");
                 mno.setFont(new Font("Arial", Font.PLAIN, 15));
-                mno.setSize(100, 20);
-                mno.setLocation(100, 150);
+                mno.setSize(300, 20);
+                mno.setLocation(50, 150);
                 c.add(mno);
                 tmno = new JTextField();
                 tmno.setFont(new Font("Arial", Font.PLAIN, 15));
                 tmno.setSize(250, 20);
-                tmno.setLocation(250, 150);
+                tmno.setLocation(350, 150);
                 c.add(tmno);
-                dob = new JLabel("When Last Seen?");
+                /*dob = new JLabel("When Last Seen?");
                 dob.setFont(new Font("Arial", Font.PLAIN, 15));
                 dob.setSize(150, 20);
                 dob.setLocation(100, 200);
@@ -54,16 +54,16 @@ public class CustomApplet extends JApplet {
                 date.setFont(new Font("Arial", Font.PLAIN, 15));
                 date.setSize(250, 20);
                 date.setLocation(250, 200);
-                c.add(date);
-                sub = new JButton("Send");
+                c.add(date);*/
+                sub = new JButton("Порахувати");
                 sub.setFont(new Font("Arial", Font.PLAIN, 15));
-                sub.setSize(100, 40);
+                sub.setSize(150, 40);
                 sub.setLocation(150, 250);
                 c.add(sub);
                 reset = new JButton("Cancel");
                 reset.setFont(new Font("Arial", Font.PLAIN, 15));
-                reset.setSize(100, 40);
-                reset.setLocation(270, 250);
+                reset.setSize(150, 40);
+                reset.setLocation(370, 250);
                 c.add(reset);
 
                 setVisible(true);
@@ -76,7 +76,7 @@ public class CustomApplet extends JApplet {
                     jFrame.setContentPane(customApplet);
                     jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                     jFrame.setVisible(true);
-                    jFrame.setPreferredSize(new Dimension(620, 400));
+                    jFrame.setPreferredSize(new Dimension(820, 400));
                     jFrame.pack();
                 } catch (Exception e){
                     e.printStackTrace();
